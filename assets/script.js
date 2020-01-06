9ff624d8bfd1cd3bb1d569b5f8d6ea84b396876f
Allison Bradley <allison.bradley@outlook.com>
2020-01-06 15:12:15 -0700

    Retrieved Yelp price point response info and pushed to page

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,21 @@
 var yelpQueryURL = "http://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=starbucks&latitude=37.786882&longitude=-122.399972"
-
+var searchBox = $("#searchBox")
 $.ajax({
     url: yelpQueryURL,
     method: "GET",
     headers: {
-    Authorization: "Bearer 2XkaLgENjEmUK7eaqNghrPWK2Y6W-vvX9unRTijv3APoGO8xHVkZoGhHuW9_NBeKRmigFk-21QV8bXdM2SfIurwR7IKq5RwXWE8xlNN7fLUQBxod9JuVSh6scp4TXnYx"
+        Authorization: "Bearer 2XkaLgENjEmUK7eaqNghrPWK2Y6W-vvX9unRTijv3APoGO8xHVkZoGhHuW9_NBeKRmigFk-21QV8bXdM2SfIurwR7IKq5RwXWE8xlNN7fLUQBxod9JuVSh6scp4TXnYx"
     }
 }).then(function(response) {
+    var price = response.businesses[0].price
     console.log(response)
+    if (price === "$") {
+        searchBox.text("$5")
+    } else if (price === "$$") {
+        searchBox.text("$20")
+    } else if (price === "$$$") {
+        searchBox.text("$50")
+    } else if (price === "$$$$") {
+        searchBox.text("$100")
+    }
 })
