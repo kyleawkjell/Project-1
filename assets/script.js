ad995b338b1352e68dacaf4933e05ed66a2ca182
Kyle Kjellberg <kyleawkjell@icloud.com>
2020-01-09 11:35:18 -0700

    added number of people option

--- assets/script.js
+++ assets/script.js
@@ -51,12 +51,8 @@ var result4 = {};
 var result5 = {};
 
 function yelpSearch() {
-<<<<<<< HEAD
-    var searchTerm = $("#userInp").val()
-=======
     searchTerm = $("#userInp").val()
     $("#userInp").val("")
->>>>>>> 548951022f74eb84672f52cf892ec9f4f379cc34
     var yelpQueryURL = `http://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${searchTerm}&latitude=${userLat}&longitude=${userLong}`
     var searchBox = $("#searchResults")
     $.ajax({
@@ -115,7 +111,7 @@ function yelpSearch() {
             detractAmt = 20
         } else if (price === "$$$") {
             avgPrice.text("The average user spends between $30 and $60 here.")
-            detractAmt = 75
+            detractAmt = 45
         } else if (price === "$$$$") {
             avgPrice.text("The average user spends more than $60 here.")
             detractAmt = 100
@@ -205,16 +201,7 @@ $("#b3").on("click", function () {
 
 $("#userSubbedBudget").on("submit", function (event) {
     event.preventDefault()
-    currentBudget = $("#budgetInp").val().split(",").join("")
-    currentBudget = parseInt(currentBudget)
-    $("#budgetTotal").text(`$${currentBudget}`);
-    showBudgetDiv()
-})
-
-$("#userSubbedBudget").on("click", function (event) {
-    console.log(event.target)
-    event.preventDefault()
-    currentBudget = $("#budgetInp").val().split(",").join("")
+    currentBudget = $(".budgetInp").val().split(",").join("")
     currentBudget = parseInt(currentBudget)
     $("#budgetTotal").text(`$${currentBudget}`);
     showBudgetDiv()
@@ -245,7 +232,7 @@ $(".material-icons").on("click", function (event) {
 searchBox.on("click", "#selectChoice", function (event) {
 
     // Local storage setting:
-    runMath()
+    // runMath()
 
     var storeChoice = $("#choiceName").text();
     var locAddress = $("#choiceAddress").text();
@@ -256,7 +243,13 @@ searchBox.on("click", "#selectChoice", function (event) {
     localStorage.setItem('budget', currentBudget);
 
     // Prompt for number of people
+    var numberBtns = `<button id="numberPeople1">All by yourself...</button> <button id="numberPeople2">It's a date!</button> <button id="numberPeople3">Third wheel yikes</button>`
 
+    searchBox.empty();
+
+    searchBox.text("Please select your number of diners: ");
+
+    searchBox.append(numberBtns);
     // showDinerNumber();
 
     // Local storage setting:
@@ -270,11 +263,30 @@ searchBox.on("click", "#selectChoice", function (event) {
 
     // Run math functionality
 
+    // $(".hideSearchArea").show();
+    // resetSearch()
+})
+
+searchBox.on("click", "#rejectChoice", function (event) {
     $(".hideSearchArea").show();
     resetSearch()
 })
 
-searchBox.on("click", "#rejectChoice", function (event) {
+searchBox.on("click", "#numberPeople1", function (event) {
+    detractAmt = (detractAmt * 1);
+    runMath();
+    $(".hideSearchArea").show();
+    resetSearch()
+})
+searchBox.on("click", "#numberPeople2", function (event) {
+    detractAmt = (detractAmt * 2);
+    runMath();
+    $(".hideSearchArea").show();
+    resetSearch()
+})
+searchBox.on("click", "#numberPeople3", function (event) {
+    detractAmt = (detractAmt * 3);
+    runMath();
     $(".hideSearchArea").show();
     resetSearch()
 })
