b320cd388505e28ca0c8fda1bda8d469b7363e36
Nick Koch <marauder30@gmail.com>
2020-01-08 17:44:28 -0700

    checkpoint for gps coord and location/address getItem print working correctly

--- assets/script.js
+++ assets/script.js
@@ -32,224 +32,6 @@ function userLocation() {
 
         var currentLocation = { lat: userLat, lng: userLong };
 
-<<<<<<< HEAD
-        console.log(currentLocation);
-        
-        // the map, centered on current location
-        var map = new google.maps.Map(
-            document.getElementById("map"), { zoom: 12, center: { lat: currentLocation.lat, lng: currentLocation.lng} });
-            
-            // the marker, positioned at current location
-            var marker = new google.maps.Marker({ position: currentLocation, map: map });
-
-            console.log(currentLocation.lat);
-            
-        })
-    }
-    
-    //Google Maps functionality:
-    
-    var lat;
-    var lng;
-    var result1 = {};
-    var result2 = {};
-    var result3 = {};
-    var result4 = {};
-    var result5 = {};
-    
-    function yelpSearch() {
-        searchTerm = $("#userInp").val()
-        $("#userInp").val("")
-        var yelpQueryURL = `http://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${searchTerm}&latitude=${userLat}&longitude=${userLong}`
-        var searchBox = $("#searchResults")
-        $.ajax({
-            url: yelpQueryURL,
-            method: "GET",
-            headers: {
-                Authorization: "Bearer 2XkaLgENjEmUK7eaqNghrPWK2Y6W-vvX9unRTijv3APoGO8xHVkZoGhHuW9_NBeKRmigFk-21QV8bXdM2SfIurwR7IKq5RwXWE8xlNN7fLUQBxod9JuVSh6scp4TXnYx"
-            }
-        }).then(function (response) {
-            console.log(response)
-            resetSearch();
-            var newBtns = `<a class="waves-effect waves-light btn purple" id="selectChoice">Sounds great!</a> <a class="waves-effect waves-light btn purple" id="rejectChoice">No way, Jose</a>`
-            $("#choiceName").text(`Your choice: ${response.businesses[0].name}`)
-            $("#choiceAddress").text(`Address: ${response.businesses[0].location.address1}`)
-            searchBox.append(newBtns)
-            var lat1 = response.businesses[0].coordinates.latitude;
-            var lng1 = response.businesses[0].coordinates.longitude;
-            var lat2 = response.businesses[1].coordinates.latitude;
-            var lng2 = response.businesses[1].coordinates.longitude;
-            var lat3 = response.businesses[2].coordinates.latitude;
-            var lng3 = response.businesses[2].coordinates.longitude;
-            var lat4 = response.businesses[3].coordinates.latitude;
-            var lng4 = response.businesses[3].coordinates.longitude;
-            var lat5 = response.businesses[4].coordinates.latitude;
-            var lng5 = response.businesses[4].coordinates.longitude;
-            
-            var lat = response.region.center.latitude;
-            var lng = response.region.center.longitude;
-            
-            var newCity = { lat: lat, lng: lng };
-            
-            var restaurant1 = { lat: lat1, lng: lng1 };
-            var restaurant2 = { lat: lat2, lng: lng2 };
-            var restaurant3 = { lat: lat3, lng: lng3 };
-            var restaurant4 = { lat: lat4, lng: lng4 };
-            var restaurant5 = { lat: lat5, lng: lng5 };
-            
-            $(`#map`).html = "";
-            var map = new google.maps.Map(
-                document.getElementById("map"), { zoom: 12, center: newCity });
-                
-                var marker1 = new google.maps.Marker({ position: restaurant1, map: map });
-                var marker2 = new google.maps.Marker({ position: restaurant2, map: map });
-                var marker3 = new google.maps.Marker({ position: restaurant3, map: map });
-                var marker4 = new google.maps.Marker({ position: restaurant4, map: map });
-                var marker5 = new google.maps.Marker({ position: restaurant5, map: map });
-                
-                var price = response.businesses[0].price;
-                var avgPrice = $("#avgPrice")
-                if (price === "$") {
-                    avgPrice.text("The average user spends $10 or less here.")
-                    detractAmt = 10
-                } else if (price === "$$") {
-                    avgPrice.text("The average user spends between $10 and $30 here.")
-                    detractAmt = 20
-                } else if (price === "$$$") {
-                    avgPrice.text("The average user spends between $30 and $60 here.")
-                    detractAmt = 75
-                } else if (price === "$$$$") {
-                    avgPrice.text("The average user spends more than $60 here.")
-                    detractAmt = 100
-                }
-                
-                
-                
-                //On-click events
-                
-                $("#searchBtn").on("click", function (event) {
-                    if ($("#userInp").val() !== "") {
-                        yelpSearch();
-                    }
-                })
-                
-                searchBox.on("click", "#selectChoice", function(event) {
-                    
-                    var storeChoice = $("#choiceName").text();
-                    var choiceAddress = $("#choiceAddress").text();
-                    runMath();
-                    
-                    localStorage.setItem('query', searchTerm);
-                    localStorage.setItem('choice', storeChoice);
-                    localStorage.setItem('place', choiceAddress);
-                    localStorage.setItem('budget', currentBudget);
-                    
-                    console.log(storeChoice);
-                    
-                    
-                    var choices = localStorage.getItem('choice');
-                    console.log(choices);
-                    
-                    $("#userChoices").append(choices);
-                    
-                })
-                
-                searchBox.on("click", "#rejectChoice", function (event) {
-                    resetSearch()
-                })
-                
-            })
-            
-        }
-        
-        function runMath() {
-            currentBudget = currentBudget - detractAmt
-            console.log(currentBudget)
-            $("#budgetTotal").text(`$${currentBudget}`)
-        }
-        
-        
-        function onLoad() {
-            if (!localStorage.getItem('budget')) {
-                console.log("nothing here!");
-            } else {
-                localStorage.getItem('budget');
-                currentBudget = localStorage.getItem('budget');
-                $(".budgetBox").show();
-                $("#budgetTotal").text(`$${currentBudget}`);
-                $(".searchCard").show();
-                $(".des").hide();
-                $(".options").hide();
-                
-                console.log(currentBudget);
-                
-            }
-        }
-
-        function historyOnLoad() {
-            // $(`#userChoices`).append = window.localStorage.getItem('choice');
-            
-            // console.log(window.localStorage.getItem('choice'));
-            
-            // userChoices.text = window.localStorage.getItem('choice');
-            
-            // console.log(userChoices.text);
-            
-            // $(`#userChoices`).prepend = userChoices.text;
-            
-            console.log(localStorage.getItem('store'));
-            
-            $(`#userChoices`).text(localStorage.getItem('store'));
-            
-            console.log(window.localStorage.choice);
-            
-            $(`#choiceAddress`).append(localStorage.getItem('place'));
-            
-            
-            
-        }
-        
-        
-        userLocation();
-        historyOnLoad();
-        onLoad();
-        
-        
-        //On-click events
-        
-        $("#searchBtn").on("click", function (event) {
-            if ($("#userInp").val() !== "") {
-                yelpSearch();
-            }
-        })
-        
-        searchBox.on("click", "#rejectChoice", function (event) {
-            resetSearch()
-        })
-        
-        
-        $("#b1").on("click", function () {
-            $(".budgetBox").show();
-            $("#budgetTotal").text("$100");
-            $(".searchCard").show();
-            $(".des").hide();
-            $(".options").hide();
-            currentBudget = 100;
-            localStorage.setItem('budget', currentBudget);
-        })
-        
-        $("#b2").on("click", function () {
-            $(".budgetBox").show();
-            $("#budgetTotal").text("$250");
-            $(".searchCard").show();
-            $(".des").hide();
-            $(".options").hide();
-            currentBudget = 250
-            localStorage.setItem('budget', currentBudget);
-        })
-        
-        $("#b3").on("click", function () {
-=======
         // the map, centered on current location
         var map = new google.maps.Map(
             document.getElementById("map"), { zoom: 12, center: currentLocation });
@@ -359,7 +141,6 @@ function onLoad() {
 
 function showBudgetDiv() {
     localStorage.setItem('budget', currentBudget);
->>>>>>> 4fdae779caa2371950c03e5e208db39a8d3cea65
     $(".budgetBox").show();
     $(".searchCard").show();
     $(".des").hide();
@@ -394,6 +175,7 @@ function historyOnLoad() {
     $(`#userChoices`).text(localStorage.getItem('store'));
 
     console.log(window.localStorage.choice);
+    $(`#choiceAddress`).text(localStorage.getItem('place'));
 }
 
 
@@ -453,11 +235,11 @@ searchBox.on("click", "#selectChoice", function (event) {
     runMath()
 
     var storeChoice = $("#choiceName").text();
-    var choiceAddress = $("#choiceAddress").text();
+    var locAddress = $("#choiceAddress").text();
 
     localStorage.setItem('query', searchTerm);
     localStorage.setItem('choice', storeChoice);
-    localStorage.setItem('place', choiceAddress);
+    localStorage.setItem('place', locAddress);
     localStorage.setItem('budget', currentBudget);
 
     // Prompt for number of people
@@ -466,11 +248,11 @@ searchBox.on("click", "#selectChoice", function (event) {
 
     // Local storage setting:
 
-    var storeSearch = searchTerm;
-    window.localStorage.setItem('query', storeSearch);
-    console.log(storeSearch);
-    window.localStorage.setItem('store', choiceName.textContent);
-    console.log(choiceName.textContent);
+    // var storeSearch = searchTerm;
+    // window.localStorage.setItem('query', storeSearch);
+    // console.log(storeSearch);
+    // window.localStorage.setItem('store', choiceName.textContent);
+    // console.log(choiceName.textContent);
 
 
     // Run math functionality
